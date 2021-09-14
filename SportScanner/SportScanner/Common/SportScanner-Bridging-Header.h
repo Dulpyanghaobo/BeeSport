81c9d7b12f2dfd88bc5e382f08125aa63701dd0e
haobo_yang <haobo_yang@intsig.net>
2021-09-14 13:54:04 +0800

    no message

---
+++ SportScanner/SportScanner/Common/SportScanner-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SportScanner-Bridging-Header.h
+//  SportScanner
+//
+//  Created by 杨皓博 on 2021/9/12.
+//
+
+#ifndef SportScanner_Bridging_Header_h
+#define SportScanner_Bridging_Header_h
+
+#import "HFSColor.h"
+#import "HFSFont.h"
+
+#endif /* SportScanner_Bridging_Header_h */
